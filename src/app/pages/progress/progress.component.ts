2044420020294b1479fb703f73069349721d7c8e
iris-at <lety_ilat8@hotmail.com>
2019-11-07 19:15:15 -0600

    Se crea barra de avance y se modifican valores con atributos de angular

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ValueTransformer } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-progress',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent implements OnInit {
 
+  porcentaje: number = 50;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  modificarPorcentaje( valor ){
+    if (this.porcentaje >  100 && valor > 0) {
+      this.porcentaje = 100;
+      return;
+    }
+
+    if (this.porcentaje < 0  && valor < 0){
+      this.porcentaje = 0;
+      return;
+    }
+    this.porcentaje = this.porcentaje + valor;
+  }
 }
